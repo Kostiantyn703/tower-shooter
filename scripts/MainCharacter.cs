8f4e3d1b9e7ae0e91debf83a5be3ae6cabfa5b7a
kostiantyn_maiuk <maiuk.kostiantyn@gmail.com>
2025-05-10 14:51:23 +0300

    Add handling states with velocity and input direction.

--- scripts/MainCharacter.cs
+++ scripts/MainCharacter.cs
@@ -1,6 +1,56 @@
 using Godot;
 using System;
 
+class CharacterState
+{
+	public virtual string GetName() { return "none"; }
+}
+
+class IdleState : CharacterState
+{
+	public override string GetName() { return "idle"; }
+}
+
+class MoveState : CharacterState
+{
+	public override string GetName() { return "move"; }
+}
+
+class AirState : CharacterState
+{
+	public override string GetName() { return "air"; }
+}
+
+class CharacterStateMachine
+{
+	public void HandleState(Vector2 velocity, Vector2 direction)
+	{
+		if (velocity.Length() == 0 && direction.Length() == 0)
+		{
+			State = new IdleState();
+		}
+		else if (velocity.X != 0 && direction.X != 0 && velocity.Y == 0)
+		{
+			State = new MoveState();
+		}
+		else if (velocity.Y != 0 )
+		{
+			State = new AirState();
+		}
+	}
+	
+	public void LogCurrentState()
+	{
+		GD.Print(State.GetName());
+	}
+	
+	CharacterState State = null;
+}
+//class HoldState : CharacterState
+//{
+	//
+//}
+
 public partial class MainCharacter : CharacterBody2D
 {
 	[Export]
@@ -11,17 +61,7 @@ public partial class MainCharacter : CharacterBody2D
 	private bool IsHold = false;
 	private Vector2 InputDirection = Vector2.Zero;
 	
-	private enum CharacterState 
-	{
-		CS_IDLE,
-		CS_LAY_DOWN,
-		CS_RUN,
-		CS_AIR,
-		CS_AIM_DOWN,
-		CS_AIM_UP,
-		CS_AIM_45,
-		CS_AIM_135
-	}
+	CharacterStateMachine StateMachine = null;
 	
 	private enum AimDirection
 	{
@@ -35,13 +75,15 @@ public partial class MainCharacter : CharacterBody2D
 		AD_RIGHT_135,	// right down
 		AD_NONE
 	}
-
-	CharacterState State = CharacterState.CS_IDLE;
+	
+	private AimDirection AimDir = AimDirection.AD_NONE;
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 	
 	public override void _Ready()
 	{
+		AimDir = AimDirection.AD_RIGHT;
+		StateMachine = new CharacterStateMachine();
 	}
 	
 	public override void _PhysicsProcess(double delta)
@@ -52,69 +94,96 @@ public partial class MainCharacter : CharacterBody2D
 		if (!IsOnFloor())
 		{
 			velocity.Y += gravity * (float)delta;
-			State = CharacterState.CS_AIR;
 		}
 		
-		ProcessInput(velocity);
-		
+		ProcessInput(ref velocity);
+		Velocity = velocity;
+		StateMachine.HandleState(Velocity, InputDirection);
+		StateMachine.LogCurrentState();
 		
 		MoveAndSlide();
 		HandleAnimation();
 		
 	}
 	
-	private void ProcessInput(Vector2 velocity)
+	private void ProcessInput(ref Vector2 velocity)
 	{
 		InputDirection = Input.GetVector("move_left", "move_right", "up", "down");
 		
-		if (Input.IsActionPressed("hold")) {
-			IsHold = true;
-		}
-		else if (Input.IsActionJustReleased("hold"))
+		//GD.Print(InputDirection.X + " " + InputDirection.Y);
+		
+		if (InputDirection.X != 0)
 		{
-			IsHold = false;
-		} 
-		
-		
-		if (InputDirection == Vector2.Zero && IsOnFloor() || (IsHold && InputDirection.X != 0)) {
-			State = CharacterState.CS_IDLE;
-		}
-		
-		if (InputDirection.X != 0 && !IsHold)
-		{
-			velocity.X = InputDirection.X * Speed;
-			if (IsOnFloor())
-			{
-				State = CharacterState.CS_RUN;
-			}
-			else
-			{
-				State = CharacterState.CS_AIR;
-			} 
+			velocity.X = InputDirection.X * Speed; 
 		}
 		else if (InputDirection.X == 0)
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
 		
-		if (IsOnFloor())
-		{
-			if (InputDirection.Y == 1) {
-				if (!IsHold)	State = CharacterState.CS_LAY_DOWN;
-				else State = CharacterState.CS_AIM_DOWN;
-			}
-			else if (InputDirection.Y == -1)
-			{
-				State = CharacterState.CS_AIM_UP;
-			}
-		}
-		
 		if (Input.IsActionJustPressed("jump"))
 		{
-			velocity.Y = JumpVelocity;
+			if (IsOnFloor())
+			{
+				velocity.Y = JumpVelocity;
+			}
 		}
 		
-		Velocity = velocity;
+		//if (IsOnFloor())
+		//{
+			//if (InputDirection.Y == 1) {
+				//if (!IsHold)	State = CharacterState.CS_LAY_DOWN;
+				//else State = CharacterState.CS_AIM_DOWN;
+			//}
+			//else if (InputDirection.Y == -1)
+			//{
+				//State = CharacterState.CS_AIM_UP;
+			//}
+		//}
+		//if (Input.IsActionPressed("hold")) {
+			//IsHold = true;
+		//}
+		//else if (Input.IsActionJustReleased("hold"))
+		//{
+			//IsHold = false;
+		//} 
+		//
+		//
+		//if (InputDirection == Vector2.Zero && IsOnFloor() || (IsHold && InputDirection.X != 0)) {
+			//State = CharacterState.CS_IDLE;
+		//}
+		//
+		//if (InputDirection.X != 0 && !IsHold)
+		//{
+			//velocity.X = InputDirection.X * Speed;
+			//if (IsOnFloor())
+			//{
+				//State = CharacterState.CS_RUN;
+			//}
+			//else
+			//{
+				//State = CharacterState.CS_AIR;
+			//} 
+		//}
+		//else if (InputDirection.X == 0)
+		//{
+			//velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		//}
+		//
+		//if (IsOnFloor())
+		//{
+			//if (InputDirection.Y == 1) {
+				//if (!IsHold)	State = CharacterState.CS_LAY_DOWN;
+				//else State = CharacterState.CS_AIM_DOWN;
+			//}
+			//else if (InputDirection.Y == -1)
+			//{
+				//State = CharacterState.CS_AIM_UP;
+			//}
+		//}
+		//
+		
+		//GD.Print(Velocity.X + " " + Velocity.Y);
 	}
 	
 	private void HandleAnimation()
@@ -130,51 +199,43 @@ public partial class MainCharacter : CharacterBody2D
 		
 		AnimatedSprite2D idleAnim = GetNode<AnimatedSprite2D>("IdleAnim");
 		
-		if (IsIdleState())
-		{
-			moveAnim.Visible = false;
-			idleAnim.Visible = true;
-		}
-		else
-		{
-			moveAnim.Visible = true;
-			idleAnim.Visible = false;
-		}
+		//if (IsIdleState())
+		//{
+			//moveAnim.Visible = false;
+			//idleAnim.Visible = true;
+		//}
+		//else
+		//{
+			//moveAnim.Visible = true;
+			//idleAnim.Visible = false;
+		//}
 		
-		switch (State) {
-			case CharacterState.CS_IDLE:
-				idleAnim.Animation = "idle";
-				break;
-			case CharacterState.CS_AIM_UP:
-				idleAnim.Animation = "look_up";
-				break;
-			case CharacterState.CS_AIM_DOWN:
-				idleAnim.Animation = "aim_down";
-				break;
-			case CharacterState.CS_LAY_DOWN:
-				moveAnim.Animation = "lay_down";
-				break;
-			case CharacterState.CS_RUN:
-				moveAnim.Animation = "run";
-				break;
-			case CharacterState.CS_AIR:
-				moveAnim.Animation = "air";
-				break;
-		}
-		
-		if (InputDirection.X != 0)
-		{
-			moveAnim.FlipH = InputDirection.X < 0;
-			idleAnim.FlipH = InputDirection.X < 0;
-		}
+		//switch (State) {
+			//case CharacterState.CS_IDLE:
+				//idleAnim.Animation = "idle";
+				//break;
+			//case CharacterState.CS_AIM_UP:
+				//idleAnim.Animation = "look_up";
+				//break;
+			//case CharacterState.CS_AIM_DOWN:
+				//idleAnim.Animation = "aim_down";
+				//break;
+			//case CharacterState.CS_LAY_DOWN:
+				//moveAnim.Animation = "lay_down";
+				//break;
+			//case CharacterState.CS_RUN:
+				//moveAnim.Animation = "run";
+				//break;
+			//case CharacterState.CS_AIR:
+				//moveAnim.Animation = "air";
+				//break;
+		//}
+		//
+		//if (InputDirection.X != 0)
+		//{
+			//moveAnim.FlipH = InputDirection.X < 0;
+			//idleAnim.FlipH = InputDirection.X < 0;
+		//}
 		
 	}
-	
-	private bool IsIdleState()
-	{
-		return State == CharacterState.CS_IDLE ||
-				State == CharacterState.CS_AIM_DOWN ||
-					State == CharacterState.CS_AIM_UP;
-	}
-	
 }
